9e89b308ade7e95b0231b15147918d1969931b1f
PriyanshuDalakoti <pdalakoti1@gmail.com>
2025-10-23 09:22:19 +0530

    ok

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // listen on all interfaces
+    port: 5173
+  }
 })
+
